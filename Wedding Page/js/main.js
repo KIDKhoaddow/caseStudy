c4dfd51fa8f153c3dbcf7a4f3d564b0e60e47c02
KIDKhoaddow <phamdangkhoa.k.i.d.6868@gmail.com>
2022-05-27 20:59:51 +0700

    ver27052022

--- Wedding Page/js/main.js
+++ Wedding Page/js/main.js
@@ -1,20 +1,46 @@
+// const {data}=require("jquery");
 
-$(window).on("load",function(){
+$(window).on("load", function () {
     // console.log("hi")
-    let slideIndex=$(".slide.active").index();
-    const slideLen=$(".slide").length;
-   
-    
-    function slideShow(){
+    let slideIndex = $(".slide.active").index();
+    const slideLen = $(".slide").length;
+
+
+    function slideShow() {
         console.log(slideIndex)
         $(".slide").removeClass("active").eq(slideIndex).addClass("active");
-        if(slideIndex==slideLen-1){
-            slideIndex=0;
+        if (slideIndex == slideLen - 1) {
+            slideIndex = 0;
         }
         else slideIndex++;
 
-        setTimeout(slideShow,5000)
+        setTimeout(slideShow, 5000)
     }
     slideShow();
 
 })
+// $(document).ready(function () {
+//     peopleFilter($(".filter-btn.active").attr("data-target"))
+//     $(".filter-btn").click(function(){
+//         if(!$(this).hasClass("active")){
+//             peopleFilter($(this).attr("data-target"))
+//         }
+       
+        
+//     })
+//     function peopleFilter(target) {
+//         console.log(target)
+//         $(".filter-btn").removeClass("active");
+//         $(".filter-btn[data-target='"+target+"']").addClass("active");
+//         $(".peopel-item").hide();
+//         $(".peo-item[data-catagory='"+"'])").fadeIn();
+//     }
+// })
+
+
+
+
+
+
+
+
