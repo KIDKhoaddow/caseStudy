63a3e2b3afaba70fc47b8e532dd80e27c1d8dee4
KIDKhoaddow <phamdangkhoa.k.i.d.6868@gmail.com>
2022-05-27 21:38:39 +0700

    ver27052022

--- Wedding Page/js/main.js
+++ Wedding Page/js/main.js
@@ -1,4 +1,18 @@
-// const {data}=require("jquery");
+function headerFixed(){
+    let scrollTop=window.scrollY;
+    let header=document.getElementById("header")
+    if(scrollTop>100)
+    {
+        header.style.position="sticky";
+    }
+    else if(scrollTop<100)
+    header.style.position="absolute"
+    setTimeout(headerFixed,1);
+}
+headerFixed();
+
+
+
 
 $(window).on("load", function () {
     // console.log("hi")
@@ -19,24 +33,59 @@ $(window).on("load", function () {
     slideShow();
 
 })
-// $(document).ready(function () {
-//     peopleFilter($(".filter-btn.active").attr("data-target"))
-//     $(".filter-btn").click(function(){
-//         if(!$(this).hasClass("active")){
-//             peopleFilter($(this).attr("data-target"))
-//         }
-       
-        
-//     })
-//     function peopleFilter(target) {
-//         console.log(target)
-//         $(".filter-btn").removeClass("active");
-//         $(".filter-btn[data-target='"+target+"']").addClass("active");
-//         $(".peopel-item").hide();
-//         $(".peo-item[data-catagory='"+"'])").fadeIn();
-//     }
-// })
 
+
+let index=0
+
+function showPicture(x)
+{
+            document.getElementById("imgPopup").src="img/gallery/large/"+x+".jpg"
+    
+    document.getElementById("show-popup").style.visibility="visible";
+    index=x
+
+}
+function closePicture(){
+    document.getElementById("show-popup").style.visibility="hidden"
+}
+
+function prev(x){
+    if(x<=6&&x>=2){
+        x--;
+        showPicture(x);
+    }
+    if(x==1){
+        index=6;
+        showPicture(6);
+    }
+}
+function next(x){
+    if(x<6&&x>0){
+        x++;
+        showPicture(x);
+    }
+    if(x==6){
+        index=1;
+        showPicture(1);
+    }
+}
+function changeBridgemaid(){
+    document.getElementById("groomsmen").className="filter-btn"
+    document.getElementById("bridesmaid").className="filter-btn active"
+    document.getElementById("image1").src="img/bridesmaid/1.jpg"
+    document.getElementById("image2").src="img/bridesmaid/2.jpg"
+    document.getElementById("image3").src="img/bridesmaid/3.jpg"
+    document.getElementById("image4").src="img/bridesmaid/4.jpg"
+
+}
+function changeGroomsmen(){
+    document.getElementById("groomsmen").className="filter-btn active"
+    document.getElementById("bridesmaid").className="filter-btn"
+    document.getElementById("image1").src="img/groomsmen/1.jpg"
+    document.getElementById("image2").src="img/groomsmen/2.jpg"
+    document.getElementById("image3").src="img/groomsmen/3.jpg"
+    document.getElementById("image4").src="img/groomsmen/4.jpg"
+}
 
 
 
